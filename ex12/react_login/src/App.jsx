b3c8d0474a3f94e125815a99c3e0824b5297182b
jade9487 <jade.hsiao95@gmail.com>
2026-05-13 14:33:59 +0800

    Add files via upload

---
+++ ex12/react_login/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CGULogin from './cgu_login';
+
+function App() {
+  return (
+    <div className="App">
+      <CGULogin />
+    </div>
+  );
+}
+
+export default App;
